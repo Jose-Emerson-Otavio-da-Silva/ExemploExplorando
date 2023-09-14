6994748b177d472a3f23e80823c0f8f2ae5f492f
José Emerson Otavio da Silva <emersonr8@hotmail.com>
2023-09-14 18:45:36 -0300

    commit

--- Program.cs
+++ Program.cs
@@ -1,25 +1,86 @@
 ﻿using ExemploExplorando.Models;
 using Microsoft.Win32.SafeHandles;
 
+new ExemploExcecao().Metdodo1();
 
 
-Pessoa p1 = new(nome: "Emerson", sobrenome: "Silva");
-Pessoa p2 = new(nome: "Eduardo", sobrenome: "Neves");
-
-// Pessoa p1 = new Pessoa();
-// Pessoa p2 = new Pessoa();
-
-// p1.Nome = "Emerson"; p1.Sobrenome = "Silva";
-// p2.Nome = "Eduardo"; p2.Sobrenome = "Neves";
 
 
-Curso cursoDeIngles = new Curso();
-cursoDeIngles.Nome = "Ingles";
-cursoDeIngles.Alunos = new List<Pessoa>();
 
-cursoDeIngles.AdicionarAluno(p1);
-cursoDeIngles.AdicionarAluno(p2);
-cursoDeIngles.ListarAlunos();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// try
+// {
+//     string[] linhas = File.ReadAllLines("Arquivos/arquivo_leitura.txt");
+
+//     foreach (string linha in linhas)
+//     {
+//         Console.WriteLine(linha);
+//     }
+// }
+// catch (FileNotFoundException ex)
+// {
+//     Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Arquivo não encontrado. {ex.Message}");
+// }
+// catch (DirectoryNotFoundException ex)
+// {
+//     Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Diretório não encontrado. {ex.Message}");
+// }
+// catch (Exception ex)
+// {
+//     Console.WriteLine($"Ocorreu um erro na leitura do arquivo. Arquivo não encontrado. {ex.Message}");
+// }
+// finally
+// {
+//     Console.WriteLine("Chegou até aqui")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Pessoa p1 = new(nome: "Emerson", sobrenome: "Silva");
+// Pessoa p2 = new(nome: "Eduardo", sobrenome: "Neves");
+
+// // Pessoa p1 = new Pessoa();
+// // Pessoa p2 = new Pessoa();
+
+// // p1.Nome = "Emerson"; p1.Sobrenome = "Silva";
+// // p2.Nome = "Eduardo"; p2.Sobrenome = "Neves";
+
+
+// Curso cursoDeIngles = new Curso();
+// cursoDeIngles.Nome = "Ingles";
+// cursoDeIngles.Alunos = new List<Pessoa>();
+
+// cursoDeIngles.AdicionarAluno(p1);
+// cursoDeIngles.AdicionarAluno(p2);
+// cursoDeIngles.ListarAlunos();
 
 
 
