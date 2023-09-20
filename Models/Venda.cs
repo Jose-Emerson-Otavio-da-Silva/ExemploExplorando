81b6eb4be12f3801e0e4d7afbdc0f506f3ceb662
José Emerson Otavio da Silva <emersonr8@hotmail.com>
2023-09-19 23:09:28 -0300

    Update Venda.cs

--- Models/Venda.cs
+++ Models/Venda.cs
@@ -17,7 +17,7 @@ namespace ExemploExplorando.Models
         }
         public int Id { get; set; }
 
-        // [JsonProperty("Nome_Produto")]
+        [JsonProperty("Nome_Produto")]
         public string Produto { get; set; }
         public decimal Preco { get; set; }
         public DateTime DataVenda { get; set; }
